555eef9f0fe6d99225f159790845ccc7e7828119
anishchapagain <anishchapagain>
2019-04-01 22:13:43 +0545

    updated
    urllib 'urlopen()'

--- Chapter2-UrllibRequests/wikipedia_content_urllib.py
+++ Chapter2-UrllibRequests/wikipedia_content_urllib.py
@@ -1,11 +1,12 @@
 import urllib.request as req
 import os
 link = "https://en.wikipedia.org/wiki/List_of_most_popular_websites"
-response = requests.get(link)
+response = req.urlopen(link)
 print(type(response))
-print(response.read())
+#print(response.read())
 
 content = response.read()
+print(content)
 #Create a html file with the content received as 'content'
 file = open(os.getcwd()+os.sep+"tests"+os.sep+"wikipopular.html","wb")
 file.write(content)
