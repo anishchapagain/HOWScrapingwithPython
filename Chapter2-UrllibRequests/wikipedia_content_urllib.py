d71d4e680681a249b0d98a39d9bb138c7d04ab51
anishchapagain <anishchapagain>
2019-04-01 22:05:00 +0545

    Updated
    Wikipedia content using requests and urllib

--- Chapter2-UrllibRequests/wikipedia_content_urllib.py
+++ Chapter2-UrllibRequests/wikipedia_content_urllib.py
@@ -1,12 +1,13 @@
-import requests
+import urllib.request as req
 import os
 link = "https://en.wikipedia.org/wiki/List_of_most_popular_websites"
 response = requests.get(link)
 print(type(response))
-content = response.content
-#print(content)
+print(response.read())
+
+content = response.read()
 #Create a html file with the content received as 'content'
-file = open(os.getcwd()+os.sep+"tests"+os.sep+"wikicontent.html","wb")
+file = open(os.getcwd()+os.sep+"tests"+os.sep+"wikipopular.html","wb")
 file.write(content)
 file.close()
 #print(content)  
