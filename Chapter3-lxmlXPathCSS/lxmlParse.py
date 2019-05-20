b996956f1fd241a551326e39a0d0878d1010a79c
anishchapagain <anishchapagain>
2019-05-20 20:51:00 +0545

    updated

--- Chapter3-lxmlXPathCSS/lxmlParse.py
+++ Chapter3-lxmlXPathCSS/lxmlParse.py
@@ -1,4 +1,11 @@
 from lxml import etree
-tree = etree.parse("simple.xml")
+tree = etree.parse("food.xml")
+
+#iter through selected name found in Tree
 for element in tree.iter('name'):
     print(element.text)
+
+#iter through selected elements found in Tree
+for element in tree.iter('name','rating','feedback'):
+    print("{} - {}".format(element.tag, element.text))
+
--- Chapter3-lxmlXPathCSS/lxmlXMLFile.py
+++ Chapter3-lxmlXPathCSS/lxmlXMLFile.py
@@ -1,8 +1,21 @@
 from lxml import etree
-xml = open("simple.xml","rb").read()
+xml = open("food.xml","rb").read()
 #tree = etree.fromstring(xml)
 #tree = etree.parse(xml)
 tree = etree.XML(xml)
-for element in tree.iter('name'):
+
+print(tree)
+print(type(tree))
+
+#iter through all elements found in Tree
+for element in tree.iter():
+    print("%s - %s" % (element.tag, element.text))
+
+#iter through selected elements found in Tree
+for element in tree.iter('price','name'):
+    print("%s - %s" % (element.tag, element.text))
+
+#iter through description
+for element in tree.iter('description'):
     print("%s - %s" % (element.tag, element.text))
 
